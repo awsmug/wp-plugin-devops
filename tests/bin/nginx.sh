54d6245ad166bd43b1ce73856083666542ff1cdf
Sven Wagener <sven@awesome.ug>
2017-08-10 18:01:27 +0200

    Test, test, test

--- tests/bin/nginx.sh
+++ tests/bin/nginx.sh
@@ -7,13 +7,13 @@ set -ex
 
 USER=$(whoami)
 PHP_VERSION=$(phpenv version-name)
-ROOT=$WH_WORDPRESS_DIR
+ROOT=$WP_DIR
 PORT=9000
 SERVER=wordpress.dev
 
 function tpl {
   sed \
-    -e "s|{DIR}|$WH_NGINX_DIR|g" \
+    -e "s|{DIR}|$NGINX_DIR|g" \
     -e "s|{USER}|$USER|g" \
     -e "s|{PHP_VERSION}|$PHP_VERSION|g" \
     -e "s|{ROOT}|$ROOT|g" \
@@ -22,20 +22,20 @@ function tpl {
     < $1 > $2
 }
 
-mkdir -p "$WH_NGINX_DIR/nginx/sites-enabled"
+mkdir -p "$NGINX_DIR/nginx/sites-enabled"
 
 # Configure the PHP handler.
 PHP_FPM_BIN="$HOME/.phpenv/versions/$PHP_VERSION/sbin/php-fpm"
-PHP_FPM_CONF="$WH_NGINX_DIR/nginx/php-fpm.conf"
+PHP_FPM_CONF="$NGINX_DIR/nginx/php-fpm.conf"
 
 # Start php-fpm.
-tpl "$WH_DIR/tests/bin/php-fpm.tpl.conf" "$PHP_FPM_CONF"
+tpl "$CURRENT_DIR/tests/bin/php-fpm.tpl.conf" "$PHP_FPM_CONF"
 "$PHP_FPM_BIN" --fpm-config "$PHP_FPM_CONF"
 
 # Build the default nginx config files.
-tpl "$WH_DIR/tests/bin/nginx.tpl.conf" "$WH_NGINX_DIR/nginx/nginx.conf"
-tpl "$WH_DIR/tests/bin/fastcgi.tpl.conf" "$WH_NGINX_DIR/nginx/fastcgi.conf"
-tpl "$WH_DIR/tests/bin/default-site.tpl.conf" "$WH_NGINX_DIR/nginx/sites-enabled/default-site.conf"
+tpl "$CURRENT_DIR/tests/bin/nginx.tpl.conf" "$NGINX_DIR/nginx/nginx.conf"
+tpl "$CURRENT_DIR/tests/bin/fastcgi.tpl.conf" "$NGINX_DIR/nginx/fastcgi.conf"
+tpl "$CURRENT_DIR/tests/bin/default-site.tpl.conf" "$NGINX_DIR/nginx/sites-enabled/default-site.conf"
 
 # Start nginx.
-nginx -c "$WH_NGINX_DIR/nginx/nginx.conf"
+nginx -c "$NGINX_DIR/nginx/nginx.conf"
