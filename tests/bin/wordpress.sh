1e141709753a79403e9b905004266c70b442a32c
Sven Wagener <sven@awesome.ug>
2017-08-10 15:30:35 +0200

    Test, test, test

--- tests/bin/wordpress.sh
+++ tests/bin/wordpress.sh
@@ -14,7 +14,7 @@ DB_USER=$2
 DB_PASS=$3
 DB_HOST=${4-localhost}
 WP_VERSION=${5-latest}
-WH_WORDPRESS_DIR=${WH_WORDPRESS_DIR-/tmp/wordpress}
+WP_DIR=${WP_DIR-/tmp/wordpress}
 
 composer install
 
@@ -22,13 +22,13 @@ PATH=$PATH:./vendor/bin:./tests/bin/
 export PATH
 
 # Download.
-mkdir -p $WH_WORDPRESS_DIR
-./vendor/bin/wp core download --force --version=$WP_VERSION --path=$WH_WORDPRESS_DIR
+mkdir -p $WP_DIR
+./vendor/bin/wp core download --force --version=$WP_VERSION --path=$WP_DIR
 
 # Create config.
-rm -f ${WH_WORDPRESS_DIR}wp-config.php
-./vendor/bin/wp core config --path=$WH_WORDPRESS_DIR --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST
+rm -f ${WP_DIR}wp-config.php
+./vendor/bin/wp core config --path=$WP_DIR --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST
 
 # Install.
-./vendor/bin/wp db create --path=$WH_WORDPRESS_DIR
-./vendor/bin/wp core install --path=$WH_WORDPRESS_DIR --url="wordpress.dev:8080" --title="wordpress.dev" --admin_user="admin" --admin_password="password" --admin_email="admin@wp.dev"
+./vendor/bin/wp db create --path=$WP_DIR
+./vendor/bin/wp core install --path=$WP_DIR --url="wordpress.dev:8080" --title="wordpress.dev" --admin_user="admin" --admin_password="password" --admin_email="admin@wp.dev"
