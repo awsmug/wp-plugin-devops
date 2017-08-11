a6174ebe0930aad48160df1536ae52756a502c3f
Sven Wagener <sven@awesome.ug>
2017-08-11 09:00:58 +0200

    Test, test, test

---
+++ bin/behat.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [[ "$BEHAT" == "1" ]]; then wordpress.sh $DB_NAME $DB_USER $DB_PASS $DB_HOST $WP_VERSION $WP_DIR; fi
+if [[ "$BEHAT" == "1" ]]; then nginx.sh; fi
+if [[ "$BEHAT" == "1" ]]; then selenium.sh; fi
+cd ..
+cp -r "$PLUGIN_SLUG" "$WP_DIR/wp-content/plugins/$PLUGIN_SLUG"
+cd "$PLUGIN_SLUG"
+wp plugin activate $PLUGIN_SLUG --path=$WP_DIR
