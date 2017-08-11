e91cd55f31c782991bd3b9885e2882a2b4786108
Sven Wagener <sven@awesome.ug>
2017-08-11 13:43:02 +0200

    Test, test, test

--- bin/init-behat.sh
+++ bin/init-behat.sh
@@ -6,11 +6,3 @@
 if [[ "$BEHAT" == "1" ]]; then init-wordpress.sh $DB_NAME $DB_USER $DB_PASS $DB_HOST $WP_VERSION $WP_DIR; fi
 if [[ "$BEHAT" == "1" ]]; then init-nginx.sh; fi
 if [[ "$BEHAT" == "1" ]]; then init-selenium.sh; fi
-
-##
-# Copy Plugin to WordPress Direcotry
-##
-cd ..
-cp -r "$PLUGIN_SLUG" "$WP_DIR/wp-content/plugins/$PLUGIN_SLUG"
-cd "$PLUGIN_SLUG"
-wp plugin activate $PLUGIN_SLUG --path=$WP_DIR
