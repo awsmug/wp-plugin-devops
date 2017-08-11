2d17c0b0727287a5a5bae69b545da15b8ffbf954
Sven Wagener <sven@awesome.ug>
2017-08-11 15:23:34 +0200

    Test, test, test

--- bin/init-nginx.sh
+++ bin/init-nginx.sh
@@ -29,13 +29,13 @@ PHP_FPM_BIN="$HOME/.phpenv/versions/$PHP_VERSION/sbin/php-fpm"
 PHP_FPM_CONF="$NGINX_DIR/nginx/php-fpm.conf"
 
 # Start php-fpm.
-tpl "$BIN_PATH/php-fpm.tpl.conf" "$PHP_FPM_CONF"
+tpl "$BIN_DIR/php-fpm.tpl.conf" "$PHP_FPM_CONF"
 "$PHP_FPM_BIN" --fpm-config "$PHP_FPM_CONF"
 
 # Build the default nginx config files.
-tpl "$BIN_PATH/nginx.tpl.conf" "$NGINX_DIR/nginx/nginx.conf"
-tpl "$BIN_PATH/fastcgi.tpl.conf" "$NGINX_DIR/nginx/fastcgi.conf"
-tpl "$BIN_PATH/default-site.tpl.conf" "$NGINX_DIR/nginx/sites-enabled/default-site.conf"
+tpl "$BIN_DIR/nginx.tpl.conf" "$NGINX_DIR/nginx/nginx.conf"
+tpl "$BIN_DIR/fastcgi.tpl.conf" "$NGINX_DIR/nginx/fastcgi.conf"
+tpl "$BIN_DIR/default-site.tpl.conf" "$NGINX_DIR/nginx/sites-enabled/default-site.conf"
 
 # Start nginx.
 nginx -c "$NGINX_DIR/nginx/nginx.conf"
