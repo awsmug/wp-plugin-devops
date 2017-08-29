2aa70603c147a9fae7834e9595fe2a19585f868a
Sven Wagener <sven@awesome.ug>
2017-08-29 10:43:13 +0200

    Changed bash scripts for running on new system

--- bin/docker/start.sh
+++ bin/docker/start.sh
@@ -4,6 +4,22 @@ echo "Starting development environment..."
 
 docker-compose -f $DOCKER_COMPOSE_FILE up -d
 
-$BIN_PATH/init.sh
+echo "Initializing project..." >&2
+
+echo "Container: $WP_CONTAINER_NAME"
+
+echo "Waiting for Server..."
+sleep 20
+
+REMOTE_PLUGIN_PATH=${PLUGIN_PATH}/wordpress/wp-content/plugins/
+docker exec ${WP_CONTAINER_NAME} mkdir -p ${REMOTE_PLUGIN_PATH}
+rsync -rv --exclude-from=${BIN_PATH}/exclude-plugin-files.txt ${PLUGIN_PATH} ${REMOTE_PLUGIN_PATH}
+docker exec ${WP_CONTAINER_NAME} wp plugin activate ${PLUGIN_SLUG} --path=/var/www/html
+
+# Custom user scripts
+if [ -f "${PLUGIN_PATH}/project.sh" ]; then
+    echo "Starting project scripts..."
+    ${PLUGIN_PATH}/project.sh
+fi
 
 echo "Finished starting webserver!"
--- bin/docker/stop.sh
+++ bin/docker/stop.sh
@@ -2,6 +2,6 @@
 
 echo "Stopping webserver"
 
-docker-compose -f "${BIN_PATH}/docker-compose.yml" down
+docker-compose -f "${PLUGIN_PATH}/docker-compose.yml" down
 
 echo "Finished stopping webserver!"
