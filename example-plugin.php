df8231d3e6c1a7646c513473e9590d2a24eea035
Sven Wagener <sven@awesome.ug>
2017-09-05 19:27:01 +0200

    Deleted Name from plugin file

--- example-plugin.php
+++ example-plugin.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: Example Plugin
-Plugin URI: https://awesome.ug
-Description: We are testing devops
-Version: 1.0
-Author: Sven Wagener
-Author URI: https://awesome-ug
+Plugin URI: https://your-url-here.com
+Description: Your Plugin description
+Version: 1.0.0
+Author: Your Name
+Author URI: https://your-plugin-url-here.com
 Min WP Version: 4.6
 Max WP Version: 4.8.1
 */
