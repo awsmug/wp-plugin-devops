5be3fcdc140967bfdaa4ded96aef659a3062b28a
Sven Wagener <sven@awesome.ug>
2017-09-11 13:00:27 +0200

    Pushing for travis

--- bin/travis/webserver.sh
+++ bin/travis/webserver.sh
@@ -10,7 +10,7 @@ DB_USER=$2
 DB_PASS=$3
 DB_HOST=${4-localhost}
 WP_VERSION=${5-latest}
-WP_DIR=${6-/tmp/wordpress}
+WP_DIR=${6-${CURRENT_DIR}/wordpress}
 
 travis-wordpress.sh $DB_NAME $DB_USER $DB_PASS $DB_HOST $WP_VERSION $WP_DIR
 travis-nginx.sh
