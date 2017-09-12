aa3f9b4394f7cbc4374b6f25d752524e7461b983
Sven Wagener <sven@awesome.ug>
2017-09-12 23:34:09 +0200

    Behat tests

--- bin/travis/travis-wordpress.sh
+++ bin/travis/travis-wordpress.sh
@@ -8,13 +8,13 @@ if [ $# -lt 3 ]; then
   echo "usage: $0 <db-name> <db-user> <db-pass> <db-host> <wp-version> <wp-dir>"
   exit 1
 fi
-
 DB_NAME=$1
 DB_USER=$2
 DB_PASS=$3
 DB_HOST=${4-localhost}
 WP_VERSION=${5-latest}
 WP_DIR=${6-${CURRENT_DIR}/wordpress}
+
 
 # Download.
 mkdir -p $WP_DIR
