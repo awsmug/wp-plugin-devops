69a794df7d703b17e33abd953eecbfce0a46baad
Sven Wagener <sven@awesome.ug>
2017-09-13 19:32:55 +0200

    Added travis to dir

--- bin/travis/travis-nginx.sh
+++ bin/travis/travis-nginx.sh
@@ -29,13 +29,13 @@ mkdir -p "$NGINX_DIR/nginx/sites-enabled"
 PHP_FPM_CONF="$NGINX_DIR/nginx/php-fpm.conf"
 
 # Start php-fpm.
-tpl "$BIN_DIR/conf/php-fpm.tpl.conf" "$PHP_FPM_CONF"
+tpl "$BIN_DIR/travis/conf/php-fpm.tpl.conf" "$PHP_FPM_CONF"
 "$PHP_FPM_BIN" --allow-to-run-as-root --fpm-config "$PHP_FPM_CONF"
 
 # Build the default nginx config files.
-tpl "$BIN_DIR/conf/nginx.tpl.conf" "$NGINX_DIR/nginx/nginx.conf"
-tpl "$BIN_DIR/conf/fastcgi.tpl.conf" "$NGINX_DIR/nginx/fastcgi.conf"
-tpl "$BIN_DIR/conf/default-site.tpl.conf" "$NGINX_DIR/nginx/sites-enabled/default-site.conf"
+tpl "$BIN_DIR/travis/conf/nginx.tpl.conf" "$NGINX_DIR/nginx/nginx.conf"
+tpl "$BIN_DIR/travis/conf/fastcgi.tpl.conf" "$NGINX_DIR/nginx/fastcgi.conf"
+tpl "$BIN_DIR/travis/conf/default-site.tpl.conf" "$NGINX_DIR/nginx/sites-enabled/default-site.conf"
 
 # Start nginx.
 nginx -c "$NGINX_DIR/nginx/nginx.conf"
