cfc9c29df43905f16090e7881baa3218ad6072cc
Sven Wagener <sven@awesome.ug>
2017-09-26 17:22:44 +0200

    Changed path in sync file

--- bin/docker/sync.sh
+++ bin/docker/sync.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rsync -r --exclude-from=${BIN_DIR}/exclude-plugin-files.txt ${PLUGIN_PATH} ${REMOTE_PLUGIN_PATH}
+rsync -r --exclude-from=${VENDOR_BIN_PATH}/exclude-plugin-files.txt ${PLUGIN_PATH} ${REMOTE_PLUGIN_PATH}
